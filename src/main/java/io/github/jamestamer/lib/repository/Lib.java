687760a292abe60ccc731a3bf7ade628773f525b
Epicgo <105683201+epicgo@users.noreply.github.com>
2022-11-19 01:37:16 -0600

    Updated Lib.java converted to abstract and methods.

---
+++ src/main/java/io/github/jamestamer/lib/repository/Lib.java
@@ -0,0 +1,10 @@
+package io.github.jamestamer.lib.repository;
+
+import java.net.URL;
+
+public abstract class Lib {
+
+    public abstract String getJarName();
+
+    public abstract URL getDownloadURL();
+}
